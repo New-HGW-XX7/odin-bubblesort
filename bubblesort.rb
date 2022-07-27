1131173c84f74a5a59126d559bf705c285043399
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-07-27 18:11:28 +0200

    Add autocorrection by rubocop

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 array = [0, 2, 1, 2, 3, 11, 8, 0, 1, 0, 99, 10]
 
 def bubblesort(arr)
-    swap = true
-    
-    while swap == true
-        swap = false
-        i = 0
-        while i < arr.length - 1
-        
-            if arr[i] > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                swap = true
-            end
-            p arr
-            i += 1
-        end
-    
+  swap = true
+
+  while swap == true
+    swap = false
+    i = 0
+    while i < arr.length - 1
+
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap = true
+      end
+      p arr
+      i += 1
     end
 
-p arr
+  end
+
+  p arr
 end
 
 bubblesort(array)
